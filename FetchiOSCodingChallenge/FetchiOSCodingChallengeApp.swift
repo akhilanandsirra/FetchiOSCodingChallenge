dd24538259e594ee365367fdb0da5af953c40db1
akhilanandsirra <akhilanand9999@gmail.com>
2023-04-15 18:49:55 -0400

    Initial Commit

---
+++ FetchiOSCodingChallenge/FetchiOSCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchiOSCodingChallengeApp.swift
+//  FetchiOSCodingChallenge
+//
+//  Created by Akhil Anand Sirra on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchiOSCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
